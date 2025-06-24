abbe955c793738992d8eadc475012fec6ef8378d
Elizabeth Nguyen <elizabethtallentnguyen@gmail.com>
2025-06-23 20:06:26 -0400

    Stop tracking config.js - added to .gitignore

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '1754290038850808',
-        clientSecret: 'c7aa17185c8a1d2756d3a992f1fe58ef'
-    }
-}
